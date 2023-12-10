fb11be00901f5de9e7a859c46cd719f9d62479d6
Grace Paclibar <graceannruthnpaclibar@iskolarngbayan.pup.edu.ph>
2023-12-10 19:20:33 +0800

    Added Profile and Settings Button (frfr)

--- app/src/main/java/com/bscpe/omcmapp/MainActivity.kt
+++ app/src/main/java/com/bscpe/omcmapp/MainActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.CalendarView;
 
 class MainActivity : AppCompatActivity() {
+    // Connects activity_main.xml here
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
+   //Displays Menu Bar
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
