97bb0bc37570ff935cdf3bc0d80661c933344a96
Grace Paclibar <graceannruthnpaclibar@iskolarngbayan.pup.edu.ph>
2024-01-14 01:35:32 +0800

    Can now open camera

--- app/src/main/java/com/bscpe/omcmapp/MainActivity.kt
+++ app/src/main/java/com/bscpe/omcmapp/MainActivity.kt
@@ -3,15 +3,33 @@ package com.bscpe.omcmapp
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.ImageButton
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val takePictureLauncher: ActivityResultLauncher<Intent> =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                // The image capture was successful
+                // You can handle the captured image here
+            }
+        }
 
     // Connects activity_main.xml
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cameraButton = findViewById<ImageButton>(R.id.scan_tab)
+
+        cameraButton.setOnClickListener {
+            openCamera()
+        }
     }
 
     fun goToProfile(view: View) {
@@ -48,6 +66,17 @@ class MainActivity : AppCompatActivity() {
     }
     fun goToSensors(view: View) {
 
+    }
+
+    private fun openCamera() {
+        val captureImageIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        if (captureImageIntent.resolveActivity(packageManager) != null) {
+            takePictureLauncher.launch(captureImageIntent)
+        } else {
+            // Handle the case where no camera app is available
+            // You can display a message or take alternative actions
+        }
     }
 
 //   //Displays Menu Bar
