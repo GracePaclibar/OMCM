cd56cdc6db8df64d1d2034d96ab1632c2ced3f50
Grace Paclibar <graceannruthnpaclibar@iskolarngbayan.pup.edu.ph>
2024-06-04 22:01:35 +0800

    smol tweaks

--- app/src/main/java/com/bscpe/omcmapp/FeedbacksActivity.kt
+++ app/src/main/java/com/bscpe/omcmapp/FeedbacksActivity.kt
@@ -5,89 +5,83 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class FeedbacksActivity : AppCompatActivity(){
 
-    private lateinit var feedbackEmailEditText: EditText
+    private lateinit var feedbackEmailEditText: TextView
     private lateinit var feedbackEditText: EditText
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var userEmail: String
+    private lateinit var userName: String
 
     override fun onCreate(savedInstanceState : Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feedbacks)
 
+        FirebaseApp.initializeApp(this)
+        firebaseAuth = FirebaseAuth.getInstance()
+        val userUid = firebaseAuth.currentUser?.uid
+        firebaseDatabase = FirebaseDatabase.getInstance()
+
         feedbackEmailEditText = findViewById(R.id.feedbackEmail)
         feedbackEditText = findViewById(R.id.feedback)
 
-        // Initializing Firebase
-        FirebaseApp.initializeApp(this)
+        userEmail = firebaseAuth.currentUser?.email.toString()
+        feedbackEmailEditText.text = userEmail
+
+        val userNameRef = firebaseDatabase.getReference("UsersData/$userUid/Profile/Name")
+
+        userNameRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                userName = dataSnapshot.getValue(String::class.java).toString()
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                println("Database error: ${databaseError.message}")
+            }
+        })
     }
 
-    fun sendFeedback(view : View) {
-        val email = feedbackEmailEditText.text.toString()
+    fun sendFeedback(view: View) {
+        val email = userEmail
         val feedback = feedbackEditText.text.toString()
 
         if (feedback.isNotEmpty() && email.isNotEmpty()) {
-            val subject = "Feedback from $email"
+            val subject = "Feedback from: $userName"
             val message = feedback
 
             val intent = Intent(Intent.ACTION_SEND).apply {
-                type = "message/rfc822"
-                putExtra(Intent.EXTRA_EMAIL, arrayOf("smarthydra16@gmail.com"))
+                type = "text/plain"
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("omcm.system@gmail.com"))
                 putExtra(Intent.EXTRA_SUBJECT, subject)
                 putExtra(Intent.EXTRA_TEXT, message)
-
-                feedbackEditText.text.clear()
-                feedbackEmailEditText.text.clear()
             }
 
             try {
-                startActivity(Intent.createChooser(intent, "Please select an email client"))
+                startActivity(Intent.createChooser(intent, "Send feedback via"))
+                feedbackEditText.text.clear()
 
             } catch (e: Exception) {
                 Toast.makeText(this, "Failed to open email client.", Toast.LENGTH_SHORT).show()
             }
-        }
-        else if (email.isEmpty()) {
+        } else if (email.isEmpty()) {
             Toast.makeText(this, "Please enter your email.", Toast.LENGTH_SHORT).show()
-        }
-        else {
+        } else {
             Toast.makeText(this, "Please enter your feedback.", Toast.LENGTH_SHORT).show()
         }
     }
 
-//    fun sendFeedback(view : View) {
-//        val email = feedbackEmailEditText.text.toString()
-//        val feedback = feedbackEditText.text.toString()
-//
-//        if (feedback.isNotEmpty() && email.isNotEmpty()) {
-//            val databaseRef = FirebaseDatabase.getInstance().reference
-//
-//            // appending a child path for feedback and email to set value
-//            val feedbackRef = databaseRef.child("Feedbacks").push() //generates UID for feedback
-//
-//            // hashmap for email and feedback
-//            val feedbackData = HashMap<String, Any>()
-//            feedbackData["feedback"] = feedback
-//            feedbackData["email"] = email
-//
-//            feedbackRef.setValue(feedbackData)
-//
-//            feedbackEditText.text.clear()
-//            feedbackEmailEditText.text.clear()
-//
-//            Toast.makeText(this, "Thank you for your feedback!", Toast.LENGTH_SHORT).show()
-//        }
-//        else if (email.isEmpty()) {
-//            Toast.makeText(this, "Please enter your email.", Toast.LENGTH_SHORT).show()
-//        }
-//        else {
-//            Toast.makeText(this, "Please enter your feedback.", Toast.LENGTH_SHORT).show()
-//        }
-//    }
+
 
     fun goToSettings(view: View) {
         val intent = Intent(this, SettingsActivity::class.java)
